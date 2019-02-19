0e09fb997080a520a1124e9f25b7f4c49aa74ec0
Vinicius Weiss Cobos <v.weisscobos@gmail.com>
2019-02-19 15:35:54 -0300

    implement auth0

---
+++ src/components/Callback.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Callback extends React.Component {
+
+    componentDidMount() {
+        //  Handle authentication if expected values are in the URL.
+        if (/access_token|id_token|error/.test(this.props.location.hash)) {
+            this.props.auth.handleAuthentication();
+        } else {
+            throw new Error("Invalid callback URL.");
+        }
+    }
+
+    render() {
+        return <h1>Loading...</h1>
+    }
+}
+
+export default Callback;
