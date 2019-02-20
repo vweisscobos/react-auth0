02b64fc54e1c5a775ed4b99343b99cd7d8a9ab6e
Vinicius Weiss Cobos <v.weisscobos@gmail.com>
2019-02-20 14:10:16 -0300

    fix typos

---
+++ src/components/Private.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Private extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            message: ""
+        }
+    }
+
+    componentDidMount() {
+        fetch('/private', {
+            headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
+        })
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error('Network response was not ok.')
+        })
+        .then(response => this.setState({ message: response.message }))
+        .catch(error => this.setState({ message: error.message }));
+    }
+
+    render() {
+        return <p>{this.state.message}</p>
+    }
+}
+
+export default Private;
