eb1a47c1949b777516ba066d2c2adb585f6bd27e
Vinicius Weiss Cobos <v.weisscobos@gmail.com>
2019-02-21 11:46:25 -0300

    put auth tokens in memory

--- src/App.js
+++ src/App.js
@@ -19,13 +19,23 @@ class App extends Component {
     super(props);
 
     this.state = {
-      auth: new Auth(this.props.history)
+      auth: new Auth(this.props.history),
+      tokenRenewalComplete: false
     };
+  }
+
+  componentDidMount() {
+    this.state.auth.renewToken(() => {
+      this.setState({ tokenRenewalComplete: true})
+    });
   }
 
   render() {
 
     const { auth } = this.state;
+
+    //  Show loading message until the token renewal check is completed.
+    if (!this.state.tokenRenewalComplete) return "Loading...";
 
     return (
       <AuthContext.Provider value={auth}>
--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -2,6 +2,13 @@ import auth0 from 'auth0-js';
 
 const REDIRECT_ON_LOGIN = 'redirect_on_login';
 
+//  Store outside class since private
+//  eslint-disable-next-line
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
 export default class Auth {
     constructor(history) {
         this.history = history;
@@ -45,40 +52,30 @@ export default class Auth {
 
     setSession = authResult => {
         //  set the time that the access token expires.
-        const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime());
-
-        const scopes = authResult.scopes || this.requestedScopes || '';
-
-        localStorage.setItem("access_token", authResult.accessToken);
-        localStorage.setItem("id_token", authResult.idToken);
-        localStorage.setItem("expires_at", expiresAt);
-        localStorage.setItem("scopes", JSON.stringify(scopes));
-    }
+        _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
+        _scopes = authResult.scopes || this.requestedScopes || '';
+        _accessToken = authResult.accessToken;
+        _idToken = authResult.idToken;
+        this.scheduleTokenRenewal();
+    };
 
     isAuthenticated = () => {
-        const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-        return new Date().getTime() < expiresAt;
-    }
+        return new Date().getTime() < _expiresAt;
+    };
 
     logout = () => {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("id_token");
-        localStorage.removeItem("expires_at");
-        localStorage.removeItem("scopes");
-        this.userProfile = null;
         this.auth0.logout({
             clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
             returnTo: 'http://localhost:3000'
         });      
-    }
+    };
 
     getAccessToken = () => {
-        const accessToken = localStorage.getItem("access_token");
-        if (!accessToken) {
+        if (!_accessToken) {
             throw new Error("No access token found.");
         }
-        return accessToken;
-    }
+        return _accessToken;
+    };
 
     getUserProfile = cb => {
         if (this.userProfile) return cb(this.userProfile);
@@ -86,12 +83,26 @@ export default class Auth {
             if (profile) this.userProfile = profile;
             cb(profile, err);
         })
-    }
+    };
 
     userHasScopes = (scopes) => {
-        const grantedScopes = (
-            JSON.parse(localStorage.getItem('scopes')) || ''
-        ).split(" ");
+        const grantedScopes = ( _scopes || '' ).split(" ");
         return scopes.every(scope => grantedScopes.includes(scope));
+    };
+
+    renewToken = (cb) => {
+      this.auth0.checkSession({}, (err, result) => {
+        if (err) {
+          console.log(`Error: ${err.error} - ${err.error_description}.`);
+        } else {
+          this.setSession(result);
+        }
+        if (cb) cb(err, result);
+      });
+    }
+
+    scheduleTokenRenewal() {
+      const delay = _expiresAt - Date.now();
+      if (delay > 0) setTimeout(() => this.renewToken(), delay);
     }
 }
